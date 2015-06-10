b5e41469579a7a6680971d0ec8704b093f5cce29
birkin <birkin.diana@gmail.com>
2015-06-10 09:43:00 -0400

    corrects module name.

--- easyrequest_app/urls_app.py
+++ easyrequest_app/urls_app.py
@@ -6,12 +6,12 @@ from django.views.generic import RedirectView
 
 urlpatterns = patterns('',
 
-    url( r'^info/$',  'easyscan_app.views.info', name=u'info_url' ),
+    url( r'^info/$',  'easyrequest_app.views.info', name=u'info_url' ),
 
-    # url( r'^shib_login/$',  'easyscan_app.views.shib_login', name=u'shib_login_url' ),
-    # url( r'^logout/$',  'easyscan_app.views.shib_logout', name=u'logout_url' ),
+    # url( r'^shib_login/$',  'easyrequest_app.views.shib_login', name=u'shib_login_url' ),
+    # url( r'^logout/$',  'easyrequest_app.views.shib_logout', name=u'logout_url' ),
 
-    # url( r'^confirmation/$',  'easyscan_app.views.confirmation', name=u'confirmation_url' ),
+    # url( r'^confirmation/$',  'easyrequest_app.views.confirmation', name=u'confirmation_url' ),
 
     url( r'^$',  RedirectView.as_view(pattern_name=u'info_url') ),
 
