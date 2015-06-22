861f1d390c7e1e010901143f1b8e67be38a02183
birkin <birkin.diana@gmail.com>
2015-06-22 10:54:39 -0400

    adds shib login and success redirect urls.

--- easyrequest_app/urls_app.py
+++ easyrequest_app/urls_app.py
@@ -10,10 +10,10 @@ urlpatterns = patterns('',
 
     url( r'^login/$',  'easyrequest_app.views.login', name=u'login_url' ),
 
-    # url( r'^shib_login/$',  'easyrequest_app.views.shib_login', name=u'shib_login_url' ),
+    url( r'^shib_login/$',  'easyrequest_app.views.shib_login', name=u'shib_login_url' ),
     # url( r'^logout/$',  'easyrequest_app.views.shib_logout', name=u'logout_url' ),
 
-    # url( r'^confirmation/$',  'easyrequest_app.views.confirmation', name=u'confirmation_url' ),
+    url( r'^confirmation/$',  'easyrequest_app.views.confirmation', name=u'confirmation_url' ),
 
     url( r'^$',  RedirectView.as_view(pattern_name=u'info_url') ),
 
