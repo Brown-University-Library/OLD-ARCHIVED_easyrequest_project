3f4728cfae20cf06c3e366b55ffd6f99d0c4a353
birkin <birkin.diana@gmail.com>
2015-08-05 11:53:45 -0400

    adds doc comment.

--- easyrequest_app/views.py
+++ easyrequest_app/views.py
@@ -64,7 +64,7 @@ def barcode_handler( request ):
         resp = barcode_handler_helper.prep_login_redirect( request )
         return resp
     login_check = barcode_handler_helper.authenticate( request.session['barcode_login_name'], request.session['barcode_login_barcode'] )
-    if not login_check:
+    if login_check not True:  # if login fails, redirect user back to login page with error messages that will display
         resp = barcode_handler_helper.prep_login_redirect( request )
         return resp
     resp = barcode_handler_helper.prep_processor_redirect( request )
