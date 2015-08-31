ef4f30964d26dbce0173bfc7d1a11c638dc7cc53
birkin <birkin.diana@gmail.com>
2015-08-31 17:13:40 -0400

    not yet used, but implements massage of pickup location env data for coming display work.

--- easyrequest_app/models.py
+++ easyrequest_app/models.py
@@ -412,6 +412,20 @@ class PickupLocation( object ):
     def __init__( self ):
         """ dct structure example: { 'ROCK': {'code': 'r0001', 'display': 'Rockefeller Library'}, etc... } """
         self.pickup_location_dct = json.loads( os.environ['EZRQST__PICKUP_LOCATION_JSON'] )
+        self.code_to_display_dct = {}
+        self.process_dct()
+
+    def process_dct( self ):
+        """ Creates another dct from the env json like: { u'sci': u'Sciences Library', u'h0001': u'John Hay Library', etc... }.
+            Triggered by __init__() """
+        new_dct = {}
+        for ( key, val ) in self.pickup_location_dct.items():
+            log.debug( 'key, `%s`' % key ); log.debug( 'val, `%s`' % val )
+            new_key = val['code']
+            new_val = val['display']
+            new_dct[ new_key ] = new_val
+        log.debug( 'new_dct, `%s`' % new_dct )
+        self.code_to_display_dct = new_dct
 
     # end class PickupLocation
 
