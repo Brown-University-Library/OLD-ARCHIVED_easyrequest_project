0d776ade88108236eb0bbcccc7ea1ac5b42d0b10
birkin <birkin.diana@gmail.com>
2015-08-31 11:05:22 -0400

    adds debugging try-except.

--- easyrequest_config/passenger_wsgi.py
+++ easyrequest_config/passenger_wsgi.py
@@ -11,41 +11,47 @@ from __future__ import unicode_literals
         env_ezrqst
      """
 
-import os, pprint, sys
-
-
-## become self-aware
-current_directory = os.path.dirname(os.path.abspath(__file__))
-
-## vars
-CONFIG_DIR = os.path.dirname( os.path.abspath(__file__) )
-PROJECT_DIR = os.path.dirname( CONFIG_DIR )  # easyrequest_project
-PROJECT_STUFF_DIR = os.path.dirname( PROJECT_DIR )
-SITE_PACKAGES_DIR = '%s/env_ezrqst/lib/python2.7/site-packages' % PROJECT_STUFF_DIR
-ACTIVATE_FILE = '%s/env_ezrqst/bin/activate_this.py' % PROJECT_STUFF_DIR
-SETTINGS_MODULE = 'easyrequest_config.settings'
-
-## virtualenv
-execfile( ACTIVATE_FILE, dict(__file__=ACTIVATE_FILE) )  # _now_ django is loaded into env
-
-## sys.path additions
-for entry in [PROJECT_DIR, PROJECT_STUFF_DIR, SITE_PACKAGES_DIR]:
- if entry not in sys.path:
-   sys.path.append( entry )
-
-## environment additions
-os.environ['DJANGO_SETTINGS_MODULE'] = SETTINGS_MODULE  # so django can access its settings
-
-## load up env vars
-SETTINGS_FILE = os.environ['EZRQST__SETTINGS_PATH']  # set in activate_this.py, and activated above
-import shellvars
-var_dct = shellvars.get_vars( SETTINGS_FILE )
-for ( key, val ) in var_dct.items():
-    os.environ[key] = val
-
-## gogogo
-from django.core.wsgi import get_wsgi_application
 try:
-    application = get_wsgi_application()
-except Exception as e:
-    print 'passenger_wsgi.py exception...'; print unicode(repr(e))
+
+    import os, pprint, sys
+
+
+    ## become self-aware
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+
+    ## vars
+    CONFIG_DIR = os.path.dirname( os.path.abspath(__file__) )
+    PROJECT_DIR = os.path.dirname( CONFIG_DIR )  # easyrequest_project
+    PROJECT_STUFF_DIR = os.path.dirname( PROJECT_DIR )
+    SITE_PACKAGES_DIR = '%s/env_ezrqst/lib/python2.7/site-packages' % PROJECT_STUFF_DIR
+    ACTIVATE_FILE = '%s/env_ezrqst/bin/activate_this.py' % PROJECT_STUFF_DIR
+    SETTINGS_MODULE = 'easyrequest_config.settings'
+
+    ## virtualenv
+    execfile( ACTIVATE_FILE, dict(__file__=ACTIVATE_FILE) )  # _now_ django is loaded into env
+
+    ## sys.path additions
+    for entry in [PROJECT_DIR, PROJECT_STUFF_DIR, SITE_PACKAGES_DIR]:
+     if entry not in sys.path:
+       sys.path.append( entry )
+
+    ## environment additions
+    os.environ['DJANGO_SETTINGS_MODULE'] = SETTINGS_MODULE  # so django can access its settings
+
+    ## load up env vars
+    SETTINGS_FILE = os.environ['EZRQST__SETTINGS_PATH']  # set in activate_this.py, and activated above
+    import shellvars
+    var_dct = shellvars.get_vars( SETTINGS_FILE )
+    for ( key, val ) in var_dct.items():
+        os.environ[key] = val
+
+    ## gogogo
+    from django.core.wsgi import get_wsgi_application
+    try:
+        application = get_wsgi_application()
+    except Exception as e:
+        print 'passenger_wsgi.py exception...'; print unicode(repr(e))
+
+    print 'pwsgi ok'
+except Exception as e2:
+    print 'passenger_wsgi.py exception e2...'; print unicode(repr(e2))
