784af2a6e66d7f5cff73ba345ade70c48100fb6f
birkin <birkin.diana@gmail.com>
2017-08-31 09:49:58 -0400

    updates shib info grab.

--- easyrequest_app/models.py
+++ easyrequest_app/models.py
@@ -354,7 +354,8 @@ class ShibChecker( object ):
         """ Grabs shib values from http-header or dev-settings.
             Called by models.ShibViewHelper.check_shib_headers() """
         shib_dict = {}
-        if 'Shibboleth-eppn' in request.META:
+        # if 'Shibboleth-eppn' in request.META:
+        if 'HTTP_SHIBBOLETH_EPPN' in request.META:
             shib_dict = self.grab_shib_from_meta( request )
         else:
             if request.get_host() == '127.0.0.1' and project_settings.DEBUG == True:
@@ -366,13 +367,25 @@ class ShibChecker( object ):
         """ Extracts shib values from http-header.
             Called by grab_shib_info() """
         shib_dict = {
-            'eppn': request.META.get( 'Shibboleth-eppn', '' ),
-            'firstname': request.META.get( 'Shibboleth-givenName', '' ),
-            'lastname': request.META.get( 'Shibboleth-sn', '' ),
-            'email': request.META.get( 'Shibboleth-mail', '' ).lower(),
-            'patron_barcode': request.META.get( 'Shibboleth-brownBarCode', '' ),
-            'member_of': request.META.get( 'Shibboleth-isMemberOf', '' ) }
+            'eppn': request.META.get( 'HTTP_SHIBBOLETH_EPPN', '' ),
+            'firstname': request.META.get( 'HTTP_SHIBBOLETH_GIVENNAME', '' ),
+            'lastname': request.META.get( 'HTTP_SHIBBOLETH_SN', '' ),
+            'email': request.META.get( 'HTTP_SHIBBOLETH_MAIL', '' ).lower(),
+            'patron_barcode': request.META.get( 'HTTP_SHIBBOLETH_BROWNBARCODE', '' ),
+            'member_of': request.META.get( 'HTTP_SHIBBOLETH_ISMEMBEROF', '' ) }
         return shib_dict
+
+    # def grab_shib_from_meta( self, request ):
+    #     """ Extracts shib values from http-header.
+    #         Called by grab_shib_info() """
+    #     shib_dict = {
+    #         'eppn': request.META.get( 'Shibboleth-eppn', '' ),
+    #         'firstname': request.META.get( 'Shibboleth-givenName', '' ),
+    #         'lastname': request.META.get( 'Shibboleth-sn', '' ),
+    #         'email': request.META.get( 'Shibboleth-mail', '' ).lower(),
+    #         'patron_barcode': request.META.get( 'Shibboleth-brownBarCode', '' ),
+    #         'member_of': request.META.get( 'Shibboleth-isMemberOf', '' ) }
+    #     return shib_dict
 
     def evaluate_shib_info( self, shib_dict ):
         """ Returns boolean.
