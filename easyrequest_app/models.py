cd11d3da1ae60bc3cc1c7afad1229e505987fc9c
birkin <birkin.diana@gmail.com>
2018-07-02 14:55:30 -0400

    enables easier testing.

--- easyrequest_app/models.py
+++ easyrequest_app/models.py
@@ -68,7 +68,7 @@ class LoginHelper( object ):
         """ Ensures app is accessed from legit source.
             Called by views.login() """
         return_val = False
-        if request.get_host() == '127.0.0.1' and project_settings.DEBUG == True:
+        if '127.0.0.1' in request.get_host() and project_settings.DEBUG == True:
             return_val = True
         referrer_host = self.get_referrer_host( request.META.get('HTTP_REFERER', 'unavailable') )
         if referrer_host in self.LEGIT_SOURCES:
