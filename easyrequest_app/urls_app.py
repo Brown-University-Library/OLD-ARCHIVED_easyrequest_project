4ef2394ff8e039f158bda416b44f36c7bd5f0590
birkin <birkin.diana@gmail.com>
2019-09-05 16:21:37 -0400

    upgrades syntax for new django version.

--- easyrequest_app/urls_app.py
+++ easyrequest_app/urls_app.py
@@ -1,38 +1,40 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import unicode_literals
-from django.conf.urls import patterns, include, url
+
+from django.conf.urls import include, url
 from django.views.generic import RedirectView
+from easyrequest_app import views
 
 
-urlpatterns = patterns('',
+urlpatterns = [
 
-    url( r'^info/$',  'easyrequest_app.views.info', name='info_url' ),
+    url( r'^info/$', views.info, name='info_url' ),
 
-    url( r'^login/$',  'easyrequest_app.views.login', name='login_url' ),  # main landing page
+    url( r'^login/$', views.login, name='login_url' ),  # main landing page
 
-    url( r'^barcode_handler/$',  'easyrequest_app.views.barcode_handler', name='barcode_handler_url' ),  # eventually (happy path) redirects to `processor/`
-    url( r'^shib_handler/$',  'easyrequest_app.views.shib_handler', name='shib_handler_url' ),  # eventually (happy path) redirects to `shib_login/`
+    url( r'^barcode_handler/$', views.barcode_handler, name='barcode_handler_url' ),  # eventually (happy path) redirects to `processor/`
+    url( r'^shib_handler/$', views.shib_handler, name='shib_handler_url' ),  # eventually (happy path) redirects to `shib_login/`
 
-    url( r'^shib_login/$',  'easyrequest_app.views.shib_login', name='shib_login_url' ),  # eventually (happy path) redirects to `processor/`
+    url( r'^shib_login/$', views.shib_login, name='shib_login_url' ),  # eventually (happy path) redirects to `processor/`
 
-    url( r'^processor/$',  'easyrequest_app.views.processor', name='processor_url' ),  # eventually (happy path) redirects to `logout/`
+    url( r'^processor/$', views.processor, name='processor_url' ),  # eventually (happy path) redirects to `logout/`
 
-    url( r'^logout/$',  'easyrequest_app.views.shib_logout', name='logout_url' ),  # eventually (happy path) redirects to `summary/`
+    url( r'^logout/$', views.shib_logout, name='logout_url' ),  # eventually (happy path) redirects to `summary/`
 
-    url( r'^summary/$',  'easyrequest_app.views.summary', name='summary_url' ),
+    url( r'^summary/$', views.summary, name='summary_url' ),
 
-    url( r'^problem/$',  'easyrequest_app.views.problem', name='problem_url' ),
+    url( r'^problem/$', views.problem, name='problem_url' ),
 
-    url( r'^stats_api/v1/$',  'easyrequest_app.views.stats_v1', name=u'stats_v1_url' ),
+    url( r'^stats_api/v1/$', views.stats_v1, name=u'stats_v1_url' ),
 
     # ====================
     # development support
     # ====================
 
-    url( r'^version/$', 'easyrequest_app.views.version', name='version_url' ),
-    url( r'^error_check/$', 'easyrequest_app.views.error_check', name='error_check_url' ),
+    url( r'^version/$', views.version, name='version_url' ),
+    url( r'^error_check/$', views.error_check, name='error_check_url' ),
 
     url( r'^$',  RedirectView.as_view(pattern_name='info_url') ),
 
-    )
+    ]
