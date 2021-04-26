b5f73904b48695854ca9be060035d7603d41209c
birkin <birkin.diana@gmail.com>
2021-04-26 10:03:12 -0400

    re-organizes urls to clarify which ones are viewable by end-users.

--- easyrequest_app/urls_app.py
+++ easyrequest_app/urls_app.py
@@ -7,9 +7,17 @@ from easyrequest_app import views
 
 urlpatterns = [
 
+    ## viewable by end-user
+
     url( r'^info/$', views.info, name='info_url' ),
 
     url( r'^login/$', views.login, name='login_url' ),  # main landing page
+
+    url( r'^summary/$', views.summary, name='summary_url' ),
+
+    url( r'^problem/$', views.problem, name='problem_url' ),
+
+    ## other
 
     url( r'^barcode_handler/$', views.barcode_handler, name='barcode_handler_url' ),  # eventually (happy path) redirects to `processor/`
     url( r'^shib_handler/$', views.shib_handler, name='shib_handler_url' ),  # eventually (happy path) redirects to `shib_login/`
@@ -19,10 +27,6 @@ urlpatterns = [
     url( r'^processor/$', views.processor, name='processor_url' ),  # eventually (happy path) redirects to `logout/`
 
     url( r'^logout/$', views.shib_logout, name='logout_url' ),  # eventually (happy path) redirects to `summary/`
-
-    url( r'^summary/$', views.summary, name='summary_url' ),
-
-    url( r'^problem/$', views.problem, name='problem_url' ),
 
     url( r'^stats_api/v1/$', views.stats_v1, name=u'stats_v1_url' ),
 
